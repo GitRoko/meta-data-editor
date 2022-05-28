0d20727a85d02cb1a77b92b0ff679110b0825c9f
Kozhukhovskyi Roman <4kroman@gmail.com>
2022-05-28 15:27:21 +0300

    add title from initial data and separate them

--- src/components/ContentData.vue
+++ src/components/ContentData.vue
@@ -1,20 +1,21 @@
 <template>
   <v-container>
-        <v-row>
-          <v-sheet color="white" elevation="0" height="100%" width="100%">
-            <v-col cols="12">
-              <v-textarea
-                v-model="dat"
-                auto-grow
-                solo
-                name="text"
-                label="Solo textarea"
-              ></v-textarea>
-              <p>{{ dat }}</p>
-            </v-col>
-          </v-sheet>
-        </v-row>
-      </v-container>
+    <v-row>
+      <v-sheet color="white" elevation="0" height="100%" width="100%">
+        <v-col cols="12">
+          <!-- <v-textarea
+            v-model="dat"
+            auto-grow
+            solo
+            name="text"
+            label="Solo textarea"
+          ></v-textarea> -->
+          <p>{{ title }}</p>
+          <p>{{ parsedData }}</p>
+        </v-col>
+      </v-sheet>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
@@ -23,15 +24,34 @@ export default {
   props: {
     init_data: String,
   },
-watch: {
-  init_data() {
-    this.dat = this.init_data
-  }
-},
+  // watch: {
+  //   init_data() {
+  //     this.data = this.init_data.split("\r\n");
+  //     this.title = this.data.find((item) => item.startsWith("#"));
+  //     this.parsedData = this.data
+  //       .filter((item) => !item.startsWith("#"))
+  //       .join("\n");
+  //   },
+  // },
+  computed: {
+    data() {
+      return this.init_data.split("\r\n");
+    },
+    title() {
+      return this.data.find((item) => item.startsWith("#"));
+    },
+    parsedData() {
+      return this.data
+        .filter((item) => !item.startsWith("#"))
+        .join("\r\n");
+    },
+  },
   data() {
     return {
-      dat: this.init_data
-    }
+      // data: "",
+      // title: "",
+      // parsedData: "",
+    };
   },
 };
 </script>
