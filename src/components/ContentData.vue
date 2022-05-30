916b59422b849cab85c0d79b50ea4e13f66745e0
Kozhukhovskyi Roman <4kroman@gmail.com>
2022-05-30 12:55:19 +0300

    add YAML package, add parsedIncomingDataToJson

--- src/components/ContentData.vue
+++ src/components/ContentData.vue
@@ -3,15 +3,15 @@
     <v-row>
       <v-sheet color="white" elevation="0" height="100%" width="100%">
         <v-col cols="12">
-          <!-- <v-textarea
-            v-model="dat"
+          <p>{{ title }}</p>
+          <v-textarea
+            v-model="textArea"
             auto-grow
             solo
             name="text"
             label="Solo textarea"
-          ></v-textarea> -->
-          <p>{{ title }}</p>
-          <p>{{ parsedData }}</p>
+          ></v-textarea>
+          <p>{{ parsedIncomingDataToJSON }}</p>
         </v-col>
       </v-sheet>
     </v-row>
@@ -19,35 +19,39 @@
 </template>
 
 <script>
+import YAML from 'yaml';
 export default {
   name: "ContentData",
   props: {
     init_data: String,
   },
-  // watch: {
-  //   init_data() {
-  //     this.data = this.init_data.split("\r\n");
+  watch: {
+    textArea() {
+      this.textArea = this.parsedIncomingDataToJSON;
   //     this.title = this.data.find((item) => item.startsWith("#"));
   //     this.parsedData = this.data
   //       .filter((item) => !item.startsWith("#"))
   //       .join("\n");
-  //   },
-  // },
+    },
+  },
   computed: {
-    data() {
+    incomingData() {
       return this.init_data.split("\r\n");
     },
-    title() {
-      return this.data.find((item) => item.startsWith("#"));
+    title() { // get title
+      return this.incomingData.find((item) => item.startsWith("#"));
     },
-    parsedData() {
-      return this.data
-        .filter((item) => !item.startsWith("#"))
+    parsedIncomingDataToJSON() {
+      const preparedData = this.incomingData;
+      const filteredData = preparedData.filter((item) => !item.startsWith("#"))
         .join("\r\n");
+
+      return JSON.stringify(YAML.parse(filteredData), null, 2);
     },
   },
   data() {
     return {
+      textArea: "",
       // data: "",
       // title: "",
       // parsedData: "",
